191f0b6afbe7ecd6913d4f25189d2dafdea51758
sarat <saratjabari905@gmail.com>
2025-06-21 19:38:54 +0300

    Add getAllPets endpoint

--- src/clients/PetClient.ts
+++ src/clients/PetClient.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
-import { handleError } from "../utils/handleErrors";
 import { IPet } from "../models/petModel";
 
-const Base_URL = 'http://localhost:300/api';
+const BaseURL = 'http://localhost:3000/api';
 
 export const getAllPets = async (): Promise<IPet[]> => {
     try{
-        const response = await axios.get<IPet[]>(`${Base_URL}/pets`);
+        const response = await axios.get<IPet[]>(`${BaseURL}/pets`);
         return response.data;
-    }
-    catch (err: any) {
-        throw err
+    }catch(err){
+        console.error('Error in getAllPets:', err);
+        throw err;
     }
 }
