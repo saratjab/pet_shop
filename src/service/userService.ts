648022abeeeba75ab71ad637a9b5c0ce88b15370
sarat <saratjabari905@gmail.com>
2025-06-26 13:47:50 +0300

    Add validation before saving the user

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -21,6 +21,24 @@ export const findUserByUsername = async (username: string): Promise<HydratedDocu
 }
 
 export const saveUser = async (user: IUser): Promise<HydratedDocument<IUser>> => {
+    if(!user.username || typeof user.username !== 'string'){
+        throw Error(`username required and should be string`);
+    }
+    if(user.role && typeof user.role !== 'string'){
+        throw Error(`role should be string`);
+    }
+    if(!user.password || typeof user.password !== 'string'){
+        throw Error(`password requried and should be string`);
+    }
+    if(!user.email || typeof user.email !== 'string'){
+        throw Error(`email required and should be string`);
+    }
+    if(user.address && typeof user.address !== 'string'){
+        throw Error(`address should be string`);
+    }
+    if(user.isActive && typeof user.isActive !== 'boolean'){
+        throw Error(`isActive should be boolean`);
+    }
     const newUser = new User( user );
     const savedUser = await newUser.save();
     if(!savedUser) throw Error(`Error saving user`);
