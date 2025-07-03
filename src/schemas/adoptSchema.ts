7e1da251f0b7ee84f35abda407a6289e6b50af83
sarat <saratjabari905@gmail.com>
2025-07-03 10:25:33 +0300

    Additional Error messages for Adopt schemas

--- src/schemas/adoptSchema.ts
+++ src/schemas/adoptSchema.ts
@@ -3,21 +3,28 @@ import { z } from 'zod';
 const objError = { required_error: "field is required" }
 
 export const adoptionSchema = z.object({
-    user_id: z.string(objError),
+    user_id: z.string(objError)
+        .length(24, 'Invalid MongoDB ObjectId'),
     pets: z.string(objError)
         .array()
         .refine(pets => pets.length > 0, { message: 'At least one pet must be selected for adoption' }),
-    payMoney: z.number().positive().default(0),
+    payMoney: z.number()
+        .positive('pay money must be positive')
+        .default(0),
 });
 
 export const paymentSchema = z.object({
-    payMoney: z.number(objError).positive(),
+    payMoney: z.number(objError)
+        .positive('pay money must be psitive'),
 });
 
 export const cancelPetsSchema = z.object({
-    pets: z.string(objError).array().nonempty(),
+    pets: z.string(objError)
+        .array()
+        .nonempty('At least one pet must be selected for cancelling'),
 });
 
 export const adoptIdParamSchema  = z.object({
-    id: z.string(objError).length(24, 'Invalid MongoDB ObjectId'),
+    id: z.string(objError)
+        .length(24, 'Invalid MongoDB ObjectId'),
 });
