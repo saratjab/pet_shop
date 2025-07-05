8adc3a8592532dee9dfc425d9c65e7d367136f23
sarat <saratjabari905@gmail.com>
2025-07-05 22:59:50 +0300

    fix bugs in types name

--- src/types/petTypes.ts
+++ src/types/petTypes.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
-import { filterPetsQuerySchema, filterQuery, fromToQuerySchema, updatePetSchema } from '../schemas/petSchema';
+import { filterPetsQuerySchema, updatePetSchema } from '../schemas/petSchema';
 
 export type updatePetType = z.infer<typeof updatePetSchema>;
 // export type queryFromTo = z.infer<typeof fromToQuerySchema>;
 // export type query = z.infer<typeof filterPetsQuerySchema>;
-export type Query = z.infer<typeof filterQuery>;
+export type Query = z.infer<typeof filterPetsQuerySchema>;
