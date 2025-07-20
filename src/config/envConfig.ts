8a25f733692383261b9cf6f629c18f7692c7cf21
sarat <saratjabari905@gmail.com>
2025-07-20 09:10:17 +0300

    Create config file

---
+++ src/config/envConfig.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+});
+
+module.exports = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    HOST: process.env.HOST || 'localhost',
+    PORT: process.env.PORT || 3000,
+    DATABASE_URL: process.env.DATABASE_URL,
+};
