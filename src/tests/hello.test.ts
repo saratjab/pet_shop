d0af16257859686ee58ab8d7f61c6176b3d8bef7
sarat <saratjabari905@gmail.com>
2025-07-27 11:35:45 +0300

    Add comments to /hello test

--- src/tests/hello.test.ts
+++ src/tests/hello.test.ts
@@ -8,9 +8,12 @@ app.get('/hello', (req, res) => {
 });
 
 describe('GET /hello', () => {
+  //? describe is a jest function that groups related tests
+  //* grouping test realted to endpoint /hello
   it('should return hello world', async () => {
-    const res = await request(app).get('/hello');
+    //? it is a jest function that defines a single test case
+    const res = await request(app).get('/hello'); //? request use a superset to simulate a real http request to express app
     expect(res.status).toBe(200);
     expect(res.text).toBe('hello world');
-  });
+  }); //? sends GET /hello request to the express app / asserts the request /
 });
