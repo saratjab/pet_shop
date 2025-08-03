a26bc492728d427a6f959bb49781babe7c08dd85
sarat <saratjabari905@gmail.com>
2025-08-03 14:00:11 +0300

    test duplicate error

--- src/tests/models/pet.model.test.ts
+++ src/tests/models/pet.model.test.ts
@@ -1,3 +1,4 @@
+import { util } from 'zod/v4/core';
 import Pet from '../../models/petModel';
 import { petBuilderData } from '../builder/petBuilder';
 
@@ -22,5 +23,14 @@ describe('Pet Model', () => {
 
   it('should throw validation errors if required fieds are missing', async () => {
     await expect(Pet.create({})).rejects.toThrow(/validation failed/i);
+  });
+
+  it('should throw duplicate key error if petTag is not unique', async () => {
+    const pet = await Pet.create(petData);
+    const petDuplicate = new Pet(petData);
+    await expect(petDuplicate.save()).rejects.toMatchObject({
+      name: 'MongoServerError',
+      code: 11000,
+    });
   });
 });
