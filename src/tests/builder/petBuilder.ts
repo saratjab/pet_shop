89ee6b5287a0d04b0a784f8c878d1bf34ffc45b6
sarat <saratjabari905@gmail.com>
2025-08-03 13:33:25 +0300

    Create pet builder

---
+++ src/tests/builder/petBuilder.ts
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker/.";
+
+export const petBuilderData = (override = {}) => {
+  return {
+    petTag: faker.string.alphanumeric(8), 
+    name: faker.animal.cat(),
+    kind: faker.animal.type(), 
+    age: faker.number.int({ min: 1, max: 10 }),
+    price: faker.number.float({ min: 10, max: 100, fractionDigits: 1 }),
+    gender: 'F', 
+    isAdopted: false,
+    ...override,
+  };
+};
