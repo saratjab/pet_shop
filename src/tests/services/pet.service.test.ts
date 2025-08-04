e0c21d00452ad193c1f7ca055f2ccbae9352794d
sarat <saratjabari905@gmail.com>
2025-08-04 13:51:32 +0300

    Add test to the right branch

---
+++ src/tests/services/pet.service.test.ts
@@ -0,0 +1,61 @@
+import logger from '../../config/logger';
+import Pet from '../../models/petModel';
+import { findPetById, findPetByPetTag } from '../../service/petService';
+import { petBuilder } from '../builder/petBuilder';
+import mongoose from 'mongoose';
+
+jest.mock('../../config/logger');
+
+describe('findPetByPetTag service', () => {
+  let mockPets: any[];
+  let petTag1 = 'tag1';
+  let petTag2 = 'tag2';
+
+  beforeEach(async () => {
+    jest.resetAllMocks();
+    mockPets = [
+      petBuilder({ petTag: petTag1 }),
+      petBuilder({ petTag: petTag2 }),
+    ];
+
+    await Pet.insertMany(mockPets);
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+    await Pet.deleteMany({});
+  });
+
+  it('should return the pet document', async () => {
+    const pet = await Pet.findOne({ petTag: petTag1 });
+
+    const result = await findPetByPetTag(petTag1);
+
+    expect(result).toEqual(pet);
+    expect(logger.debug).toHaveBeenCalledWith(
+      'Searching for pet by petTag: tag1'
+    );
+    expect(logger.debug).toHaveBeenCalledWith('pet found with petTag: tag1');
+  });
+
+  it('should throw error if pet not found', async () => {
+    await expect(findPetByPetTag('invalid-tag')).rejects.toThrow(
+      'pet not found'
+    );
+    expect(logger.debug).toHaveBeenCalledWith(
+      'Searching for pet by petTag: invalid-tag'
+    );
+    expect(logger.warn).toHaveBeenCalledWith(
+      'Pet not found with petTag: invalid-tag'
+    );
+  });
+
+  it('should call Pet.findOne with the correct petTag', async () => {
+    const spy = jest.spyOn(Pet, 'findOne');
+
+    await findPetByPetTag(petTag1);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith({ isAdopted: false, petTag: petTag1 });
+  });
+});
