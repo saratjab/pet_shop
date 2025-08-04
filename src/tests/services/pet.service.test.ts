2490028f7c60f6ba4306f0e3c1a59e7f7dc05755
sarat <saratjabari905@gmail.com>
2025-08-04 11:57:54 +0300

    test Happy path findPetById

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,15 +1,44 @@
 import logger from '../../config/logger';
 import Pet from '../../models/petModel';
+import { findPetById } from '../../service/petService';
 import { petBuilder } from '../builder/petBuilder';
+import mongoose from 'mongoose';
 
 jest.mock('../../config/logger');
 
 describe('findPetById service', () => {
   let mockPets: any[];
+  let id1: mongoose.Types.ObjectId;
+  let id2: mongoose.Types.ObjectId;
 
   beforeEach(async () => {
-    mockPets = [petBuilder({ id: '1' }), petBuilder({ id: '2' })];
+    jest.resetAllMocks();
+    id1 = new mongoose.Types.ObjectId('507f1f77bcf86cd799439011');
+    id2 = new mongoose.Types.ObjectId('667f1f77bcf86cd799439011');
+    mockPets = [petBuilder({ _id: id1 }), petBuilder({ _id: id2 })];
+
+    await Pet.insertMany(mockPets); // Using insertMany for better performance
   });
 
-  
+  afterEach(async () => {
+    jest.clearAllMocks();
+    await Pet.deleteMany({});
+  });
+
+  it('should return the pet document', async () => {
+    const result = await findPetById(id1.toString());
+
+    expect(result._id).toEqual(id1);
+    expect(result.petTag).toBe(mockPets[0].petTag);
+    expect(result.name).toBe(mockPets[0].name);
+    expect(result.kind).toBe(mockPets[0].kind);
+    expect(result.age).toBe(mockPets[0].age);
+    expect(result.price).toBe(mockPets[0].price);
+    expect(result.description).toBe(mockPets[0].description);
+
+    expect(logger.debug).toHaveBeenCalledWith(
+      `Searching for pet by ID: ${id1}`
+    );
+    expect(logger.debug).toHaveBeenCalledWith(`pet found with ID: ${id1}`);
+  });
 });
