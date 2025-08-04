775fb2a80fcd45e6c3449c226b5bc10c969194da
sarat <saratjabari905@gmail.com>
2025-08-04 11:14:25 +0300

    setup test  findPetById

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,2 +1,15 @@
 import logger from '../../config/logger';
 import Pet from '../../models/petModel';
+import { petBuilder } from '../builder/petBuilder';
+
+jest.mock('../../config/logger');
+
+describe('findPetById service', () => {
+  let mockPets: any[];
+
+  beforeEach(async () => {
+    mockPets = [petBuilder({ id: '1' }), petBuilder({ id: '2' })];
+  });
+
+  
+});
