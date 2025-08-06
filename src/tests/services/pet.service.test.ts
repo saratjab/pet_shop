6b5ebce6cf85e67bade46c7cba7b0df6a9a37306
sarat <saratjabari905@gmail.com>
2025-08-06 11:09:09 +0300

    resetUp and test happy path

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,46 +1,31 @@
 import logger from '../../config/logger';
-import Pet from '../../models/petModel';
-import { savePet, updatePets } from '../../service/petService';
+import { updatePets } from '../../service/petService';
 import { petBuilder } from '../builder/petBuilder';
-import { petFixture } from '../fixture/petFixture';
 
 jest.mock('../../config/logger');
 
-describe('updatePets Service', () => {
+describe('updatePets service', () => {
   let mockPet: any;
   let savePet: jest.Mock;
-
-  beforeAll(async () => {
-    jest.resetModules(); // clears any cached modules
-
-    jest.mock('../../service/petService', () => ({
-      savePet: jest.fn(), // only mock the savePet functoin
-    }));
-
-    savePet = jest.fn().mockImplementation(async (pet) => pet);
-  });
-
   beforeEach(() => {
     mockPet = petBuilder();
+    savePet = jest.fn().mockImplementation((pet) => pet);
+  });
+
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
-  afterAll(() => {
-    jest.clearAllMocks();
-    jest.resetModules();
-    jest.unmock('../../service/petService');
-  });
-
-  it('should update pet details', async () => {
+  it('should update pet successfuly', async () => {
     const updatedData = {
-      ...mockPet,
-      name: 'Updated Pet',
+      name: 'updated name',
       age: 3,
     };
+
     const updatedPet = await updatePets(mockPet, updatedData);
 
     expect(updatedPet).toBeDefined();
-    expect(updatedPet.name).toBe('Updated Pet');
+    expect(updatedPet.name).toBe('updated name');
     expect(updatedPet.age).toBe(3);
     expect(logger.debug).toHaveBeenCalledWith(
       `Updating pet: ${updatedPet.petTag}`
