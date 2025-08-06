338220e604cebcbb1fb6fbfe308e6083d1bd6bec
sarat <saratjabari905@gmail.com>
2025-08-06 08:37:08 +0300

    Test saving valid pet

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,5 +1,6 @@
 import logger from '../../config/logger';
 import Pet from '../../models/petModel';
+import { savePet } from '../../service/petService';
 import { petBuilder } from '../builder/petBuilder';
 import { petFixture } from '../fixture/petFixture';
 
@@ -7,16 +8,10 @@ jest.mock('../../config/logger');
 
 describe('savePet service', () => {
   let mockPets: any[];
-  let petTag1 = 'tag1';
-  let petTag2 = 'tag2';
 
   beforeEach(async () => {
     jest.resetAllMocks();
-    mockPets = [
-      petBuilder(petFixture),
-    ];
-
-    await Pet.insertMany(mockPets);
+    mockPets = [petBuilder(petFixture)];
   });
 
   afterEach(async () => {
@@ -24,4 +19,21 @@ describe('savePet service', () => {
     await Pet.deleteMany({});
   });
 
+  it('should save a new pet to the database', async () => {
+    const pet = await savePet(mockPets[0]);
+
+    expect(pet._id).toBeDefined();
+    expect(pet.petTag).toBe(mockPets[0].petTag);
+    expect(pet.name).toBe(mockPets[0].name);
+    expect(pet.kind).toBe(mockPets[0].kind);
+    expect(pet.age).toBe(mockPets[0].age);
+    expect(pet.price).toBe(mockPets[0].price);
+    expect(pet.description).toBe(mockPets[0].description);
+
+    const petInDb = await Pet.findById(pet._id);
+    expect(petInDb).toBeDefined();
+
+    expect(logger.debug).toHaveBeenCalledWith('saving new pet to database');
+    expect(logger.debug).toHaveBeenCalledWith(`Pet saved with ID: ${pet._id}`);
+  });
 });
