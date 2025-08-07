59e89539ebb89c84742a0764946ec4da07a73d40
sarat <saratjabari905@gmail.com>
2025-08-07 13:56:03 +0300

    Add test handling DB error & check order of logger

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,55 +1,61 @@
+import mongoose from 'mongoose';
 import logger from '../../config/logger';
 import Pet from '../../models/petModel';
 import { findPetById } from '../../service/petService';
 import { petBuilder } from '../builder/petBuilder';
-import mongoose from 'mongoose';
 
 jest.mock('../../config/logger');
 
 describe('findPetById service', () => {
-  let mockPets: any[];
-  let id1: mongoose.Types.ObjectId;
-  let id2: mongoose.Types.ObjectId;
+  let mockPet: any;
+  let mockedLogger: any;
+  let id: mongoose.Types.ObjectId;
 
   beforeEach(async () => {
-    jest.resetAllMocks();
-    id1 = new mongoose.Types.ObjectId('507f1f77bcf86cd799439011');
-    id2 = new mongoose.Types.ObjectId('667f1f77bcf86cd799439011');
-    mockPets = [petBuilder({ _id: id1 }), petBuilder({ _id: id2 })];
+    // jest.resetAllMocks();//? it reset mock implementations created jest.fn() to return it to a normal function
+    //? reset mock calls history
+    mockedLogger = logger as jest.Mocked<typeof logger>;
+    id = new mongoose.Types.ObjectId('507f1f77bcf86cd799439011');
+    mockPet = petBuilder({ _id: id });
 
-    await Pet.insertMany(mockPets); // Using insertMany for better performance
+    await Pet.insertMany(mockPet); // Using insertMany for better performance
   });
 
   afterEach(async () => {
-    jest.clearAllMocks();
+    jest.clearAllMocks(); // it didn't clear the mocks in the top-level
+    //? it clears mock implementations created jest.fn()
+    //? clears mock calls history
+    //? clears instances jest.spyOn()
     await Pet.deleteMany({});
   });
 
-  it('should return the pet document', async () => {
-    const result = await findPetById(id1.toString());
+  it('should return the pet document by ID', async () => {
+    const result = await findPetById(id.toString());
 
-    expect(result._id).toEqual(id1);
-    expect(result.petTag).toBe(mockPets[0].petTag);
-    expect(result.name).toBe(mockPets[0].name);
-    expect(result.kind).toBe(mockPets[0].kind);
-    expect(result.age).toBe(mockPets[0].age);
-    expect(result.price).toBe(mockPets[0].price);
-    expect(result.description).toBe(mockPets[0].description);
+    expect(result._id).toEqual(id);
+    expect(result.petTag).toBe(mockPet.petTag);
+    expect(result.name).toBe(mockPet.name);
+    expect(result.kind).toBe(mockPet.kind);
+    expect(result.age).toBe(mockPet.age);
+    expect(result.price).toBe(mockPet.price);
+    expect(result.description).toBe(mockPet.description);
 
-    expect(logger.debug).toHaveBeenCalledWith(
-      `Searching for pet by ID: ${id1}`
+    expect(mockedLogger.debug.mock.calls[0][0]).toBe(
+      `Searching for pet by ID: ${id}`
     );
-    expect(logger.debug).toHaveBeenCalledWith(`pet found with ID: ${id1}`);
+    expect(mockedLogger.debug.mock.calls[1][0]).toBe(
+      `pet found with ID: ${id}`
+    );
   });
 
   it('should throw an error if pet not found', async () => {
     const nonExistentId = new mongoose.Types.ObjectId().toString();
 
     await expect(findPetById(nonExistentId)).rejects.toThrow('pet not found');
-    expect(logger.debug).toHaveBeenCalledWith(
+    expect(mockedLogger.debug.mock.calls[0][0]).toBe(
       `Searching for pet by ID: ${nonExistentId}`
     );
-    expect(logger.warn).toHaveBeenCalledWith(
+    expect(mockedLogger.warn.mock.calls[0][0]).toBe(
       `Pet not found with ID: ${nonExistentId}`
     );
   });
@@ -58,7 +64,7 @@ describe('findPetById service', () => {
     const invalidId = 'invalid-id';
 
     await expect(findPetById(invalidId)).rejects.toThrow(/invalid-id/i);
-    expect(logger.debug).toHaveBeenCalledWith(
+    expect(mockedLogger.debug.mock.calls[0][0]).toBe(
       `Searching for pet by ID: ${invalidId}`
     );
   });
@@ -66,9 +72,16 @@ describe('findPetById service', () => {
   it('should call Pet.findById with the correct ID', async () => {
     const spy = jest.spyOn(Pet, 'findById'); // This lets you track calls to Pet.findById without replacing its implementation.
 
-    await findPetById(id1.toString());
+    await findPetById(id.toString());
 
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith(id1.toString());
+    expect(spy).toHaveBeenCalledWith(id.toString());
+  });
+
+  it('should handle database error', async () => {
+    const mockFindById = jest
+      .spyOn(Pet, 'findById')
+      .mockRejectedValue(new Error('DB error'));
+    await expect(findPetById(id.toString())).rejects.toThrow('DB error');
   });
 });
