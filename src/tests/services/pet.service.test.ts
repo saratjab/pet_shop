b35d12cc9542dbd6c5655713a25c438e71080fa9
sarat <saratjabari905@gmail.com>
2025-08-07 09:11:59 +0300

    test filter by kind

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,20 +1,44 @@
 import Pet from '../../models/petModel';
 import { petBuilder } from '../builder/petBuilder';
+import { getAllPets } from '../../service/petService';
+import logger from '../../config/logger';
 
 jest.mock('../../config/logger');
 
 describe('getAllPets service', () => {
   let mockPets: any;
-  beforeEach( async () => {
-    mockPets = [
-        petBuilder({}),
-        petBuilder({}),
-        petBuilder({}),
-    ];
-    Pet.find = jest.fn().mockReturnValue(mockPets);
-    Pet.countDocuments = jest.fn().mockReturnValue(mockPets.length);
+  let mockPagination: any;
 
-    await Pet.insertMany(mockPets);
+  let sortMock: jest.Mock;
+  let skipMock: jest.Mock;
+  let limitMock: jest.Mock;
+  
+  beforeEach(async () => {
+    mockPets = [
+      petBuilder({ kind: 'kind' }),
+      petBuilder({ kind: 'kind' }),
+      petBuilder({ kind: 'other' }),
+    ];
+
+    mockPagination = {
+      limit: 5,
+      sortBy: 'name' as const,
+      order: 1,
+      page: 1,
+    }; // need to match the service's expected query structure
+
+    sortMock = jest.fn().mockReturnThis();
+    skipMock = jest.fn().mockReturnThis();
+    limitMock = jest.fn();
+
+    Pet.find = jest.fn().mockReturnValue({
+      sort: sortMock,
+      skip: skipMock,
+      limit: limitMock,
+    });
+    Pet.countDocuments = jest.fn();
+
+    // await Pet.insertMany(mockPets); useleless - mocking find, countDocument
   });
 
   afterEach(() => {
@@ -22,5 +46,24 @@ describe('getAllPets service', () => {
     jest.resetAllMocks();
   });
 
+  it('should getAllPets based on their kind', async () => {
+    const filteredPets = mockPets.filter((pet: any) => pet.kind === 'kind');
 
+    limitMock.mockResolvedValue(filteredPets);
+    (Pet.countDocuments as jest.Mock).mockResolvedValue(mockPets.length);
+
+    const { pets, total } = await getAllPets({
+      ...mockPagination,
+      kind: 'kind',
+    });
+
+    expect(Pet.find).toHaveBeenCalledWith({ kind: 'kind' });
+    expect(pets).toEqual(filteredPets);
+    expect(total).toBe(mockPets.length);
+
+    expect(logger.debug).toHaveBeenCalledWith('Filtering pets with query');
+    expect(logger.info).toHaveBeenCalledWith(
+      `Filtered ${pets.length} pets (Total: ${total})`
+    );
+  });
 });
