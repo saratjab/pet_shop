0bfbe5392bc5f6b4a942d1122856c2389613520b
sarat <saratjabari905@gmail.com>
2025-08-07 14:06:04 +0300

    Add test handling DB error

--- src/tests/services/pet.service.test.ts
+++ src/tests/services/pet.service.test.ts
@@ -1,24 +1,18 @@
 import logger from '../../config/logger';
 import Pet from '../../models/petModel';
-import { findPetById, findPetByPetTag } from '../../service/petService';
+import { findPetByPetTag } from '../../service/petService';
 import { petBuilder } from '../builder/petBuilder';
-import mongoose from 'mongoose';
 
 jest.mock('../../config/logger');
 
 describe('findPetByPetTag service', () => {
-  let mockPets: any[];
-  let petTag1 = 'tag1';
-  let petTag2 = 'tag2';
+  let mockPet: any;
+  let mockedLogger: any;
+  let petTag = 'tag1';
 
   beforeEach(async () => {
-    jest.resetAllMocks();
-    mockPets = [
-      petBuilder({ petTag: petTag1 }),
-      petBuilder({ petTag: petTag2 }),
-    ];
-
-    await Pet.insertMany(mockPets);
+    mockedLogger = logger as jest.Mocked<typeof logger>;
+    ((mockPet = petBuilder({ petTag })), await Pet.insertMany(mockPet));
   });
 
   afterEach(async () => {
@@ -27,25 +21,27 @@ describe('findPetByPetTag service', () => {
   });
 
   it('should return the pet document', async () => {
-    const pet = await Pet.findOne({ petTag: petTag1 });
+    const pet = await Pet.findOne({ petTag: petTag });
 
-    const result = await findPetByPetTag(petTag1);
+    const result = await findPetByPetTag(petTag);
 
     expect(result).toEqual(pet);
-    expect(logger.debug).toHaveBeenCalledWith(
+    expect(mockedLogger.debug.mock.calls[0][0]).toBe(
       'Searching for pet by petTag: tag1'
     );
-    expect(logger.debug).toHaveBeenCalledWith('pet found with petTag: tag1');
+    expect(mockedLogger.debug.mock.calls[1][0]).toBe(
+      'pet found with petTag: tag1'
+    );
   });
 
   it('should throw error if pet not found', async () => {
     await expect(findPetByPetTag('invalid-tag')).rejects.toThrow(
       'pet not found'
     );
-    expect(logger.debug).toHaveBeenCalledWith(
+    expect(mockedLogger.debug.mock.calls[0][0]).toBe(
       'Searching for pet by petTag: invalid-tag'
     );
-    expect(logger.warn).toHaveBeenCalledWith(
+    expect(mockedLogger.warn.mock.calls[0][0]).toBe(
       'Pet not found with petTag: invalid-tag'
     );
   });
@@ -53,9 +49,14 @@ describe('findPetByPetTag service', () => {
   it('should call Pet.findOne with the correct petTag', async () => {
     const spy = jest.spyOn(Pet, 'findOne');
 
-    await findPetByPetTag(petTag1);
+    await findPetByPetTag(petTag);
 
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith({ isAdopted: false, petTag: petTag1 });
+    expect(spy).toHaveBeenCalledWith({ isAdopted: false, petTag: petTag });
+  });
+
+  it('should handle database error', async () => {
+    jest.spyOn(Pet, 'findOne').mockRejectedValue(new Error('DB error'));
+    await expect(findPetByPetTag(petTag)).rejects.toThrow('DB error');
   });
 });
