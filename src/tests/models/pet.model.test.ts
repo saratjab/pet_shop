9f0a7acf010b2a70f80bcbc4c60938e3d376aa56
sarat <saratjabari905@gmail.com>
2025-08-07 11:29:33 +0300

    Add some tests

--- src/tests/models/pet.model.test.ts
+++ src/tests/models/pet.model.test.ts
@@ -3,6 +3,7 @@ import { petBuilderData } from '../builder/petBuilder';
 
 describe('Pet Model', () => {
   let petData: any;
+
   beforeAll(() => {
     petData = petBuilderData();
   });
@@ -21,19 +22,6 @@ describe('Pet Model', () => {
     expect(pet.updatedAt).toBeDefined();
   });
 
-  it('should throw validation errors if required fields are missing', async () => {
-    await expect(Pet.create({})).rejects.toThrow(/validation failed/i);
-  });
-
-  it('should throw duplicate key error if petTag is not unique', async () => {
-    const pet = await Pet.create(petData);
-    const petDuplicate = new Pet(petData);
-    await expect(petDuplicate.save()).rejects.toMatchObject({
-      name: 'MongoServerError',
-      code: 11000,
-    });
-  });
-
   it('should set default value true to isActive', async () => {
     const pet = await Pet.create({ ...petData, isActive: undefined });
     expect(pet.isAdopted).toBe(false);
@@ -46,14 +34,30 @@ describe('Pet Model', () => {
     expect(pet.petTag).toBe(petTag.toLowerCase());
   });
 
-  it('should gender be either M or F', async () => {
-    const pet = new Pet({ ...petData, gender: 'invalid gender' });
-    await expect(pet.save()).rejects.toThrow(/validation failed/i);
-  });
-
   it('should allow optional description field', async () => {
     const pet = await Pet.create({ ...petData, description: 'A lovely cat' });
     expect(pet.description).toBe('A lovely cat');
+  });
+
+  it('should accept M for gender', async () => {
+    const pet = new Pet({ ...petData, gender: 'M' });
+    expect(pet).toBeDefined();
+    expect(pet.gender).toBe('M');
+  });
+
+  it('should accept F for gender', async () => {
+    const pet = new Pet({ ...petData, gender: 'F' });
+    expect(pet).toBeDefined();
+    expect(pet.gender).toBe('F');
+  });
+
+  it('should throw validation errors if required fields are missing', async () => {
+    await expect(Pet.create({})).rejects.toThrow(/validation failed/i);
+  });
+
+  it('should throw error if gender is neither M or F', async () => {
+    const pet = new Pet({ ...petData, gender: 'invalid-gender' });
+    await expect(pet.save()).rejects.toThrow(/validation failed/i);
   });
 
   it('should fail if price is negative', async () => {
@@ -64,5 +68,29 @@ describe('Pet Model', () => {
   it('should fail if age is negative', async () => {
     const invalidPet = new Pet({ ...petData, age: -1 });
     await expect(invalidPet.save()).rejects.toThrow(/validation failed/i);
+  });
+
+  it('should fail if age is not a number', async () => {
+    const invalidPet = new Pet({ ...petData, age: 'invalid-age' });
+    await expect(invalidPet.save()).rejects.toThrow(/validation failed/i);
+  });
+
+  it('should fail if price is not a number', async () => {
+    const invalidPet = new Pet({ ...petData, price: 'invalid-price' });
+    await expect(invalidPet.save()).rejects.toThrow(/validation failed/i);
+  });
+
+  it('should fail if isAdopted is not a boolean', async () => {
+    const invalidPet = new Pet({ ...petData, isAdopted: 'invalid-price' });
+    await expect(invalidPet.save()).rejects.toThrow(/validation failed/i);
+  });
+
+  it('should throw duplicate key error if petTag is not unique', async () => {
+    const pet = await Pet.create(petData);
+    const petDuplicate = new Pet(petData);
+    await expect(petDuplicate.save()).rejects.toMatchObject({
+      name: 'MongoServerError',
+      code: 11000,
+    });
   });
 });
