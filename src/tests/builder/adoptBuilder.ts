4ca9201e410d44c07c9f3d814ae53ff602467b8a
sarat <saratjabari905@gmail.com>
2025-08-14 10:26:01 +0300

    fix adoptBuilder

--- src/tests/builder/adoptBuilder.ts
+++ src/tests/builder/adoptBuilder.ts
@@ -1,14 +1,9 @@
 import { faker } from '@faker-js/faker/.';
-import mongoose from 'mongoose';
 
 export const adoptBuilder = (override = {}) => {
   return {
-    user_id: new mongoose.Types.ObjectId(
-      faker.string.hexadecimal({ length: 24 })
-    ),
-    pets: [
-      new mongoose.Types.ObjectId(faker.string.hexadecimal({ length: 24 })),
-    ],
+    user_id: faker.string.hexadecimal({ length: 24 }),
+    pets: [faker.string.hexadecimal({ length: 24 })],
     payMoney: faker.number.float({ min: 10, max: 50 }),
     total: faker.number.float({ min: 50, max: 100 }),
     statu: faker.helpers.arrayElement(['pending', 'completed', 'cancelled']),
