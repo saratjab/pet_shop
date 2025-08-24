229ab9e8852142b4dce1b2c10eb02beada8d8446
sarat <saratjabari905@gmail.com>
2025-08-24 13:58:50 +0300

    fix bug in type

--- src/types/adoptTypes.ts
+++ src/types/adoptTypes.ts
@@ -1,6 +1,6 @@
 import type { z } from 'zod';
 
-import type { adoptionSchema } from '../schemas/adoptSchema';
+import type { adoptResponseSchema, adoptionSchema } from '../schemas/adoptSchema';
 
 export type createAdopt = z.infer<typeof adoptResponseSchema>;
 export type adopt = z.infer<typeof adoptionSchema>;
